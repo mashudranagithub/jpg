a3ec0b7a737e845a0ee829a7fd5a1dddfa690dba
mashudranagithub <mashudranabitm@github.com>
2020-03-23 12:18:51 +0600

    jpgsph staff page correction complete

--- jpgsph-staffs.php
+++ jpgsph-staffs.php
@@ -31,7 +31,6 @@
 								<th>SI</th>
 								<th>Name</th>
 								<th>Designation</th>
-								<th>Biography</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -39,61 +38,51 @@
 								<td>1</td>
 								<td>Sabina Faiz Rashid, PhD</td>
 								<td>Dean & Professor</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>2</td>
 								<td>Professor Malabika Sarker</td>
 								<td>Associate Dean & Director Center of Excellence for Science of Implementation and Scale up</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>3</td>
 								<td>Malay Kanti Mridha</td>
 								<td>Associate Professor and Director, CoE for NCD & Nutrition</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>4</td>
 								<td>Mr. Ilias Mahmud</td>
 								<td>Assistant Professor</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>5</td>
 								<td>Md. Tanvir Hasan</td>
 								<td>Assistant Professor</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>6</td>
 								<td>Prof. Syed Masud Ahmed</td>
 								<td>Professor & Director, CoE-HS & UHC</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>7</td>
 								<td>Prof. Zahidul Quayyum</td>
 								<td>Professor & Co-Director of research</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>8</td>
 								<td>Dr. Nepal Chandra Dey</td>
 								<td>Adjunct Professor</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>9</td>
 								<td>Prof. Kaosar Afsana</td>
 								<td>Professor</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>10</td>
 								<td>AKM Mazharul Islam</td>
 								<td>Adjunct Professor</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 						</tbody>
 					</table>
@@ -107,7 +96,6 @@
 								<th>SI</th>
 								<th>Name</th>
 								<th>Designation</th>
-								<th>Biography</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -115,29 +103,30 @@
 								<td>1</td>
 								<td>Tahsin Madani Hossain</td>
 								<td>Assistant MPH Coordinator, Administration & Student Affairs</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>2</td>
 								<td>Md. Koushik Ahmed</td>
 								<td>Deputy Director</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>3</td>
 								<td>Md. Humayun Kabir</td>
 								<td>Sr. Program Officer</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 							<tr>
 								<td>4</td>
 								<td>Fauzul Kabir</td>
 								<td>Asst. MPH Coordinator</td>
-								<td><a href="#">Download</a></td>
 							</tr>
 						</tbody>
 					</table>
 				</div><!-- single employee category end -->
+				<!-- single employee category start -->
+				<div class="single-employee-category">
+					<h4>Under Construction ..........</h4>
+				</div><!-- single employee category end -->
+
 			</div>
 		</div>
 	</div>
