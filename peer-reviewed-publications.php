55e85cecf0ccc91036041be1d4a74e53bcee7ba1
mashudranagithub <mashudranabitm@gmail.com>
2020-07-08 13:38:18 +0600

    Peer reviewed publication and pdf files complete

--- peer-reviewed-publications.php
+++ peer-reviewed-publications.php
@@ -1,1 +1,28 @@
-<?php echo"This is the all Peer Reviewed Publications page."?>
+<?php include('partials/header.php') ?>
+
+
+<div class="page-heading text-center">
+	<h2>Peer-reviewed Publications</h2>
+</div>
+
+
+<!-- Research Projects Start Here -->
+<section id="Pdf-files" class="gray">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<a href="vendor/files/research/peer_reviewed_publications/Peer-Reviewed_Publications_Jan-June2-2020.pdf" class="pdf-link" target="_blank">January - June 2, 2020: (PDF)</a>
+			</div>
+			<div class="col-md-6">
+				<a href="vendor/files/research/peer_reviewed_publications/PUBLICATIONS_2019_v12.pdf" class="pdf-link" target="_blank">January - December 2019: (PDF)</a>
+			</div>
+			<div class="col-md-6">
+				<a href="vendor/files/research/peer_reviewed_publications/Publications_2018_FinalV3.pdf" class="pdf-link" target="_blank">January - December 2018: (PDF)</a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Research Projects End Here -->
+
+
+<?php include('partials/footer.php') ?>
--- research-journal.php
+++ research-journal.php
@@ -7,29 +7,29 @@
 
 
 <!-- Research Projects Start Here -->
-<section id="Research-journals" class="gray">
+<section id="Pdf-files" class="gray">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/PUBLICATION/PUBLICATIONS_2019_v12.pdf" class="journal-link" target="_blank">2019</a>
+				<a href="http://bracjpgsph.org/images/PUBLICATION/PUBLICATIONS_2019_v12.pdf" class="pdf-link" target="_blank">2019</a>
 			</div>
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/NOTICE_BOARD/Publications_2018_FinalV3.pdf" class="journal-link" target="_blank">2018</a>
+				<a href="http://bracjpgsph.org/images/NOTICE_BOARD/Publications_2018_FinalV3.pdf" class="pdf-link" target="_blank">2018</a>
 			</div>
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/Journal/Journal_Article__2017_New.pdf" class="journal-link" target="_blank">2017</a>
+				<a href="http://bracjpgsph.org/images/Journal/Journal_Article__2017_New.pdf" class="pdf-link" target="_blank">2017</a>
 			</div>
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/PUBLICATION/Journal_Articles_2016.pdf" class="journal-link" target="_blank">2016</a>
+				<a href="http://bracjpgsph.org/images/PUBLICATION/Journal_Articles_2016.pdf" class="pdf-link" target="_blank">2016</a>
 			</div>
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/PUBLICATION/Journal_Articles_2015_V2.pdf" class="journal-link" target="_blank">2015</a>
+				<a href="http://bracjpgsph.org/images/PUBLICATION/Journal_Articles_2015_V2.pdf" class="pdf-link" target="_blank">2015</a>
 			</div>
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/Journal/Journal%20articles_2014.pdf" class="journal-link" target="_blank">2014</a>
+				<a href="http://bracjpgsph.org/images/Journal/Journal%20articles_2014.pdf" class="pdf-link" target="_blank">2014</a>
 			</div>
 			<div class="col-md-3">
-				<a href="http://bracjpgsph.org/images/Journal/brac_university_journal_2012.pdf" class="journal-link" target="_blank">2012</a>
+				<a href="http://bracjpgsph.org/images/Journal/brac_university_journal_2012.pdf" class="pdf-link" target="_blank">2012</a>
 			</div>
 		</div>
 	</div>
